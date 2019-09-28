c5528fce07aa3d69de0ca547af6efa41fbec800b
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-28 20:44:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,19 @@
-def nyc_pigeon_organizer(pigeon_data)
-  final_pigeon_organizer = {}
-  pigeon_data.each do |(color_gender_lives,values)|
-    values.each do |(all_stats,all_names)|
-      all_names.each do |name|
-        if final_pigeon_organizer[name] == nil
-          #creating intial keys for final_pigeon_organizer
-          final_pigeon_organizer[name] = {}
-        end
-        if final_pigeon_organizer[name][color_gender_lives] == nil
-          final_pigeon_organizer[name][color_gender_lives] = []
-        end
-      end
-      final_pigeon_organizer[name][color_gender_lives].push(all_stats.to_s)
-    end
-  end
-  return final_pigeon_organizer
+def nyc_pigeon_organizer(data)
+	final_pigeon_hash = {}
+	data.each do |(color_gender_lives,value)|
+
+		value.each do |(all_stats,all_names)|
+			all_names.each do |name|
+				if final_pigeon_hash[name] == nil
+					final_pigeon_hash[name] = {}
+				end
+				if final_pigeon_hash[name][color_gender_lives] == nil
+					final_pigeon_hash[name][color_gender_lives] = []
+				end
+				final_pigeon_hash[name][color_gender_lives].push(all_stats.to_s)
+			end
+			
+		end
+	end
+	puts final_pigeon_hash
 end
