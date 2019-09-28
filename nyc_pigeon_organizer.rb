0b93acd7463b2e99331619e190f98a0a5743136a
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-28 19:32:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_pigeon_organizer = {}
+  
+  data.each do |(color_gender_lives,values)|
+    values.each do |(all_stats,all_names)|
+      all_names.each do |name|
+        if final_pigeon_organizer[name] == nil
+          #creating intial keys for final_pigeon_organizer
+          final_pigeon_organizer[name] = {}
+        end
+        if final_pigeon_organizer[name][color_gender_lives] == nil
+          final_pigeon_organizer[name][color_gender_lives] = []
+        end
+      end
+      final_pigeon_organizer[name][color_gender_lives].push(all_stats.to_s)
+    end
+  end
+  return final_pigeon_organizer
 end
